a1522e0a7a8a6c33034a299e87b4c1c1c980c2f4
Ryota Murai <35632215+rmuraix@users.noreply.github.com>
2025-06-17 17:16:52 +0900

    feat: add shadcn/ui

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,18 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import tailwindcss from "@tailwindcss/vite";
+
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://tpu-kanglabs.github.io",
   base: "/ub-moji",
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  integrations: [react()],
 });
