f28ecb9513bf561c4b8db745720423dc803ee738
Ryota Murai <35632215+rmuraix@users.noreply.github.com>
2026-03-15 00:37:59 +0900

    chore: reset project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,39 +1,15 @@
 // @ts-check
-import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
-
+import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-
-import partytown from "@astrojs/partytown";
-
-import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://tpu-kanglabs.github.io",
   base: "/ub-moji",
-
-  i18n: {
-    defaultLocale: "en",
-    locales: ["en", "ja"],
-    routing: {
-      prefixDefaultLocale: false,
-    },
-  },
-
   vite: {
     plugins: [tailwindcss()],
   },
-
-  integrations: [
-    react(),
-    partytown({ config: { forward: ["dataLayer.push"] } }),
-    sitemap({
-      i18n: {
-        defaultLocale: "en",
-        locales: { en: "en", ja: "ja" },
-      },
-    }),
-  ],
+  integrations: [react()],
 });
